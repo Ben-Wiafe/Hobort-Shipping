212aa17250f65d11cda0669f94c17ff1878f0d0e
azay <azay.genius8@gmail.com>
2022-12-13 10:06:53 +0000

    past shipment form created

--- src/components/loginFiles/Create_from_past.jsx
+++ src/components/loginFiles/Create_from_past.jsx
@@ -3,21 +3,31 @@ import React from 'react'
 const Create_from_past = ({style}) => {
   return (
     <div style={style}>
-      <div style={{backgroundColor:'white', padding:'15px 15px', minHeight:'240px', width:'700px'}}>
-      <p style={{color:'#333'}}>
-        <em>Once you start creating shipments, you'll see them here.</em>
-      </p>
+    <div style={{backgroundColor:'white', padding:'15px 15px', minHeight:'240px', width:'700px'}}>
+    <small><em>Once you start creating shipments, you'll see them here</em></small>
+    <br></br>
+    <br/>
+    <p style={{color:'#333'}}>
+      <h3>Use a Past to create a Shipment Fast!</h3>
+    </p>
+    <ul style={{padding:'20px', fontSize:'15px'}}>
+      <li><p>This convenient feature helps you <b>save time</b> and <b>steps</b> creating shipments</p></li>
+      <li><p>You can <b>quickly access</b> and <b>reuse</b> Past shipment details - so <b>no need to re-enter</b> each time</p></li>
+      <li><p>Ideal you need to ship the same types of shipments, items, use the addressed, packaging, delivery options and more</p></li>
+    </ul>
+    <br/>
+    <p style={{padding:'20px', fontSize:'16px',}}>
+      <a href="#" style={{ textDecoration:'none'}}>Using Past Shipments</a>
+    </p>
 
-
-
-      </div>
-      
-      
-      
-      
-      
-      
-      </div>
+    </div>
+    
+    
+    
+    
+    
+    
+    </div>
   )
 }
 
