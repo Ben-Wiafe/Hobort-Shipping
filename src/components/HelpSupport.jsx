6e8b68d1e2dc4fe5f4d22591d5abd59284632fb0
azay <azay.genius8@gmail.com>
2022-12-29 11:25:43 +0000

    changed table to flex

--- src/components/HelpSupport.jsx
+++ src/components/HelpSupport.jsx
@@ -2,90 +2,112 @@ import React, { useState } from "react";
 import Header from "./Header";
 import Login from "./Login";
 import "./helpsupport.css";
-
+import { Link } from "react-router-dom";
+import ShippingAdvice from "./helpCard/ShippingAdvice";
+// import CustomClearance from "./helpCard/CustomClearance";
+// import ContactUs from "./helpCard/ContactUs";
+// import Faq from "./helpCard/Faq";
 const HelpSupport = () => {
   const [modalOpen, setModalOpen] = useState(false);
-  const [showCards,setShowCards] = useState(false);
-const [selected, setSelected] = useState(false);
-  const handleClick1 = () => {
-    setSelected(true);
-    setShowCards(true);
-  }
-  const handleClick2 = () => {
-    setSelected(t);
-    setShowCards(true);
-  }
-  
-  const handleClick3 = () => {
-    setSelected(true);
-    setShowCards(true);
-  }
-  
-  const handleClick4 = () => {
-    setSelected(true);
-    setShowCards(true);
-  }
-  
-  
+  const [helpCards, setHelpCards] = useState(false);
+  const [customCards, setCustomCards] = useState(false);
+  const [faqCards, setFaqCards] = useState(false);
+  const [contactCards, setContactCards] = useState(false);
+
+  const [selected, setSelected] = useState(1);
+  const handleClick1 = index => {
+    setHelpCards(true);
+    setCustomCards(false);
+    setFaqCards(false);
+    setContactCards(false);
+    setSelected(index);
+  };
+  const handleClick2 = index => {
+    setCustomCards(true);
+    setHelpCards(false);
+    setFaqCards(false);
+    setContactCards(false);
+    setSelected(index);
+  };
+  const handleClick3 = index => {
+    setFaqCards(true);
+    setHelpCards(false);
+    setContactCards(false);
+    setCustomCards(false);
+    setSelected(index);
+  };
+  const handleClick4 = index => {
+    setContactCards(true);
+    setHelpCards(false);
+    setFaqCards(false);
+    setCustomCards(false);
+    setSelected(index);
+  };
+
   return (
     <div style={{ backgroundColor: "white" }}>
-      <Header onButtonClick={() => setModalOpen(!modalOpen)} /> {modalOpen && <Login onClose={() => setModalOpen(false)} />}
+      <Header onButtonClick={() => setModalOpen(!modalOpen)} /> {modalOpen && <Login onClose={() => setModalOpen(false)} />}{" "}
       <div style={{ width: "85%", margin: "auto", marginTop: "40px", padding: "10px" }}>
-        <h1 style={{ margin: "2px 0px 20px", fontSize: "35px", lineHeight: "0.75", color: "#000" }}>Help and Support </h1>{" "}
+        <h1 style={{ margin: "2px 0px 20px", fontSize: "35px", lineHeight: "0.75", color: "#000" }}> Help and Support </h1>{" "}
         <br />
-        <table
+        <div
           style={{
-            width: "99%",
-            borderCollapse: "separate",
-            borderRadius: "3.5px",
-            borderSpacing: "0",
-            tableLayout: "fixed",
+            width: "90%",
+            flexWrap: "nowrap",
+            display: "flex",
             textAlign: "center",
-            boxShadow: "0 0 15px rgb(0 0 0 / 20%)",
-            display: "table",
-            fontSize: "14px",
-            marginLeft: "auto",
-            marginRight: "auto",
-            lineHeight: "14px",
-            borderColor: "grey",
-
+            borderRadius: "3.5px"
           }}
+          className="link"
         >
-          <tbody>
-            <tr>
-              <td onClick={handleClick1} className={selected ? 'tabb' : 'tab-title'}>
-                {" "}
-                <div  style={{padding:'9px'}}> Shipping Advice </div>
-              </td>
-              <td onClick={handleClick2} className={selected ? 'tabb' : 'tab-title'}>
-              <div>Customs Clearance Advice </div>
-              </td>{" "}
-              <td onClick={handleClick3} className={selected ? 'tabb' : 'tab-title'}>
-                {" "}
-                <div >FAQs</div>{" "}
-              </td>{" "}
-              <td onClick={handleClick4} className={selected ? 'tabb' : 'tab-title'}>
-                {" "}
-                <div >Contact Us</div>{" "}
-              </td>
-            </tr>{" "}
-          </tbody>{" "}
-          {showCards && <HelpCard title="Hello Card" content="Non officia sunt labore do quis mollit nisi anim Lorem cupidatat commodo ut commodo ipsum."/>}
-
-        </table>{" "}
+          <div>
+            <Link
+              to="/help-support/helpcard"
+              onClick={handleClick1}
+              className={location.pathname === "/help-support/helpcard" ? " defaul activeBottom" : "default"}
+            >
+              Shipping Advice
+            </Link>
+          </div>
+          <div>
+            <Link
+              to={"/help-support/customcard"}
+              onClick={handleClick2}
+              className={location.pathname === "/help-support/customcard" ? "defaul activeBottom" : "default"}
+            >
+              Customs Clearance Advice
+            </Link>
+          </div>
+          <div>
+            <Link
+              to={"/help-support/faq"}
+              onClick={handleClick3}
+              className={location.pathname === "/help-support/faq" ? "defaul activeBottom" : "default"}
+            >
+              FAQ
+            </Link>
+          </div>
+          <div>
+            <Link
+              to={"/help-support/contactcard"}
+              onClick={handleClick4}
+              className={location.pathname === "/help-support/contactcard" ? "defaul activeBottom" : "default"}
+            >
+              Contact Us
+            </Link>
+          </div>
+        </div>{" "}
+        <br />
+        <br />
+        {helpCards && <ShippingAdvice />}
+        {/* {contactCards && <ContactUs />}
+        {faqCards && <Faq />}
+        {customCards && <CustomClearance />} */}
+      
+        
       </div>{" "}
     </div>
   );
 };
 
 export default HelpSupport;
-
-export function HelpCard({ title, content }) {
-  return (
-    <div style={{ border: "1px solid black", padding: "10px" }}>
-      {" "}
-     <h3> {title} </h3> <br />
-      <p> {content} </p>{" "}
-    </div>
-  );
-}
